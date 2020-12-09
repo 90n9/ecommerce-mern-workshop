cc3b617c516044d29217721a5ba50c76d8e1cd23
Narathip Harijiratiwong <narathip@worldrewardsolutions.com>
2020-12-10 05:01:49 +0700

    remove localstorage because of bug that it not update in realtime

--- client/src/reducers/CartReducer.js
+++ client/src/reducers/CartReducer.js
@@ -5,10 +5,7 @@ import {
   CART_RESET
 } from "../actions/types";
 
-const LOCALSTORAGE_ECOMCART = 'ecomcart'
-const ecomCart = localStorage.getItem(LOCALSTORAGE_ECOMCART);
-
-const INIT_STATE = (ecomCart) ? JSON.parse(ecomCart) : {
+const INIT_STATE = {
   carts: []
 };
 
@@ -16,39 +13,31 @@ export default (state = INIT_STATE, action) => {
   const { payload } = action;
   switch (action.type) {
     case CART_ADD: {
-      const newState = {
+      return {
         ...state,
         carts: (state.carts.filter(cart => cart.product.id === payload.product.id).length > 0) ?
           state.carts.map(cart => (cart.product.id === payload.product.id) ? { ...payload, qty: cart.qty + payload.qty } : cart)
             .filter(cart => cart.qty > 0) :
           [...state.carts, payload]
       };
-      localStorage.setItem(LOCALSTORAGE_ECOMCART, JSON.stringify(newState));
-      return newState;
     }
     case CART_UPDATE: {
-      const newState = {
+      return {
         ...state,
         carts: state.carts.map(cart => (cart.product.id === payload.product.id) ? payload : cart)
           .filter(cart => cart.qty > 0)
       };
-      localStorage.setItem(LOCALSTORAGE_ECOMCART, JSON.stringify(newState));
-      return newState;
     }
     case CART_DELETE: {
-      const newState = {
+      return {
         ...state,
         carts: state.carts.filter(cart => cart.product.id !== payload.product.id)
       };
-      localStorage.setItem(LOCALSTORAGE_ECOMCART, JSON.stringify(newState));
-      return newState;
     }
     case CART_RESET: {
-      const newState = {
+      return {
         carts: []
       };
-      localStorage.setItem(LOCALSTORAGE_ECOMCART, JSON.stringify(newState));
-      return newState;
     }
     default:
       return state;
