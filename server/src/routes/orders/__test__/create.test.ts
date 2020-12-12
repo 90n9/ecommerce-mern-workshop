2ad2b6bcf17ef1b3cdf44ed932d43110a133bf68
Narathip Harijiratiwong <narathip@worldrewardsolutions.com>
2020-12-12 14:08:49 +0700

    update create order test to have seed product and use to create new order

--- server/src/routes/orders/__test__/create.test.ts
+++ server/src/routes/orders/__test__/create.test.ts
@@ -170,20 +170,34 @@ it('create a order with valid inputs', async () => {
   let orders = await Order.find({});
   expect(orders.length).toEqual(0);
 
+  //Create product data
+  const seedResponse = await request(app)
+    .post(`/products/seed`)
+    .send({
+      amount: 10
+    });
+
+  const id = seedResponse.body[0].id;
+  const price = seedResponse.body[0].price;
+  const qty = 2;
+  const products_price = price * qty;
+  const tax_price = products_price * 0.07;
+  const total_price = products_price * 1.07;
+
   await request(app)
     .post('/orders')
     .send({
-      products_price: 1000,
-      tax_price: 70,
-      total_price: 1070,
+      products_price: products_price,
+      tax_price: tax_price,
+      total_price: total_price,
       shipping_name: "Narathip Harijiratiwong",
       shipping_mobile: "0840794234",
       shipping_address: "The KEY BTS Wutthakat",
       products: [
         {
-          "price": 1000,
-          "qty": 1,
-          "id": "productid"
+          id,
+          price,
+          qty
         }
       ]
     })
@@ -191,9 +205,9 @@ it('create a order with valid inputs', async () => {
 
   orders = await Order.find({});
   expect(orders.length).toEqual(1);
-  expect(orders[0].products_price).toEqual(1000);
-  expect(orders[0].tax_price).toEqual(70);
-  expect(orders[0].total_price).toEqual(1070);
+  expect(orders[0].products_price).toEqual(products_price);
+  expect(orders[0].tax_price).toEqual(tax_price);
+  expect(orders[0].total_price).toEqual(total_price);
   expect(orders[0].shipping_name).toEqual("Narathip Harijiratiwong");
   expect(orders[0].shipping_mobile).toEqual("0840794234");
   expect(orders[0].shipping_address).toEqual("The KEY BTS Wutthakat");
